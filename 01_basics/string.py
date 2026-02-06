4efaae5855e4512619fc209bccf3fadb4a8251d4
shuaibkhandev <skhan.csit@gmail.com>
2026-02-06 12:23:57 +0500

    work with lsit

---
+++ 01_basics/string.py
@@ -0,0 +1,14 @@
+# chai = "Masala Chai";
+# quantity = 2;
+# resp = "I Ordered {} cups of {}."
+# print(resp.format(quantity, chai));
+
+
+# list = ["apple", "mango", "banana"];
+# print(", ".join(list))
+
+chai = "Masala Chai";
+# for letter in chai:
+#     print(letter)
+
+print("Masala" in chai);
