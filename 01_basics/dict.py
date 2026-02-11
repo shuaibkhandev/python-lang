b911a4c64ac8e878411978e0e74c2206cbb02c1e
shuaibkhan_unitedsol <shuaib.khan@unitedsol.net>
2026-02-11 11:35:48 +0500

    work with dictionary

---
+++ 01_basics/dict.py
@@ -0,0 +1,58 @@
+chai_types = {"masala":"Spicy", "ginger":"Zesty", "green":"Mild"};
+# print(chai_types);
+# print(chai_types["ginger"]);
+# print(chai_types.get("ginger"))
+# chai_types["green"] = "Fresh";
+# print(chai_types);
+
+# for chai in chai_types:
+#     print("key =>", chai, "value =>", chai_types[chai])
+
+# for key, value in chai_types.items():
+#     print(key, value)
+
+# if "masala" in chai_types:
+#     print("Masala chai is available")
+
+# print(len(chai_types))
+    
+# chai_types["early grey"] = "Citrus"
+# print(chai_types)
+
+# print(chai_types.pop('ginger'))
+# print(chai_types)
+# print(chai_types.popitem())
+# print(chai_types)
+
+# chai_types2 = chai_types
+# chai_types3 = chai_types.copy()
+# print(chai_types)
+# print(chai_types2)
+# print(chai_types3)
+# chai_types2['newchai'] = 'New Chai'
+# print(chai_types)
+# print(chai_types2)
+# print(chai_types3)
+# chai_types3['newchai2'] = "New Chai 2"
+# print(chai_types)
+# print(chai_types2)
+# print(chai_types3)
+
+
+
+
+# coffeshop = {
+#     "chai_types" : {"masala":"Spicy", "ginger":"Zesty", "green":"Mild"},
+#     "employes" : 4
+# }
+# print(coffeshop["chai_types"]["masala"])
+
+# sqaured_num = {x:x**2 for x in range(6)}
+# print(sqaured_num)
+# sqaured_num.clear()
+
+dict_keys = ["masala", "ginger", "green"]
+default_value = "Delicious"
+
+new_dict = dict.fromkeys(dict_keys, default_value)
+print(new_dict)
