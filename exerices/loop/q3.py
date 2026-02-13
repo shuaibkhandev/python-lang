f92afd5d7774319b9f356cb61a77f6b673b9d388
shuaibkhan_unitedsol <shuaib.khan@unitedsol.net>
2026-02-13 14:21:14 +0500

    work with loop examples

---
+++ exerices/loop/q3.py
@@ -0,0 +1,6 @@
+str = 'python';
+reversed_str = "";
+
+for char in str:
+    reversed_str = char + reversed_str;
+print(reversed_str)
---
+++ exerices/loop/q4.py
@@ -0,0 +1,6 @@
+
+while True:
+    number = int(input("Please Enter a number between 1 to 10: "));
+    if number <= 10 and number >= 1:
+        print(number)
+        break
